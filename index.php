0a7b1af8d5ace84e2390eb97730b69c136aa1acf
Heba Shahid <hs563@njit.edu>
2017-10-22 17:46:08 -0400

    added

--- index.php
+++ index.php
@@ -1,43 +1,152 @@
 <?php
+echo "inside upload";
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["tmp_name"]);
+$FileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if file is a csv file
+print_r($_FILES);
+if(isset($_POST["submit"])) {
+   $fileName=$_FILES["fileToUpload"]["name"];
+   move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], 'uploads/' . $_FILES["fileToUpload"]["name"]);
+header("Location: http://web.njit.edu/~hs563/IS601Project1/index.php?page=htmlTable&filename=".$_FILES["fileToUpload"]["name"]);
+  echo "inside if";
+}
+//turn on debugging messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
-  $obj = new main();
-  $text = "my text";
-  $cars = array("Volvo", "BMW", "Toyota");
-  $obj->printthis($cars);
-  
-  $array = array(1,2,3,4,5,6,7);
-  $obj->printArray($array);
 
-  class main {
+//instantiate the program object
 
-    public function __construct() {
-
-      echo 'String and Array Function</br>';
-
+//Class to load classes it finds the file when the progrm starts to fail for calling a missing class
+class Manage {
+    public static function autoload($class) {
+        //you can put any file name or directory here
+        include $class . '.php';
     }
+}
 
-    public function printthis($car) {
-      echo '<h1>indexed array</h1>';
-            $cars = array("Volvo", "BMW", "Toyota");
-      echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
-      echo '<hr>'
-    }
+spl_autoload_register(array('Manage', 'autoload'));
 
-    public function printArray($array) {
-      echo '<h1>array print function</h1>';
-      print_r($array);
-      echo '<hr>';
+//instantiate the program object
+$obj = new main();
+
+
+class main {
+    
+    public function __construct()
+    {
+        //print_r($_REQUEST);
+        //set default page request when no parameters are in URL
+        $pageRequest = 'homepage';
+        //check if there are parameters
+        if(isset($_REQUEST['page'])) {
+            //load the type of page the request wants into page request
+            $pageRequest = $_REQUEST['page'];
+        }
+        //instantiate the class that is being requested
+        $page = new $pageRequest;
+        
+        
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $page->get();
+        } else {
+            $page->post();
+        }
+        
     }
     
-    public function __destruct() {
+}
 
-      echo '</br> I\'m Done';
-
+abstract class page {
+    protected $html;
+    
+    public function __construct()
+    {
+        $this->html .= '<html>';
+        $this->html .= '<link rel="stylesheet" href="styles.css">';
+        $this->html .= '<body>';
     }
+    public function __destruct()
+    {
+        $this->html .= '</body></html>';
+        echo($this->html);
+    }
+    
+    public function get() {
+        echo 'default get message';
+    }
+    
+    public function post() {
+        print_r($_POST);
+    }
+}
+class htmlTable extends page {
 
-
-  }
-
-
-
+public function get(){
+$csvFile=$_REQUEST["filename"];
+//echo $csvFile;
+$row = 1;
+if (($handle = fopen("uploads/".$csvFile, "r")) !== FALSE) {
+    
+    echo '<table border="1">';
+    
+    while (($data = fgetcsv($handle)) !== FALSE) {
+        $num = count($data);
+        if ($row == 1) {
+            echo '<thead><tr>';
+        }else{
+            echo '<tr>';
+        }
+        
+        for ($c=0; $c < $num; $c++) {
+            //echo $data[$c] . "<br />\n";
+            if(!isset($data[$c])) {
+               $value = "&nbsp;";
+            }else{
+               $value = $data[$c];
+            }
+            if ($row == 1) {
+                echo '<th>'.$value.'</th>';
+            }else{
+                echo '<td>'.$value.'</td>';
+            }
+        }
+        
+        if ($row == 1) {
+            echo '</tr></thead><tbody>';
+        }else{
+            echo '</tr>';
+        }
+        $row++;
+    }
+    
+    echo '</tbody></table>';
+    fclose($handle);
+}
+}
+}
+class homepage extends page
+{
+    
+    public function get()
+    {
+        $form = '<form action="upload.php" method="post" enctype="multipart/form-data">';
+        echo $form;
+        $form = '<input type="file" name="fileToUpload" id="fileToUpload">';
+        echo $form;
+        $form = '<input type="submit" value="Upload CSV file" name="submit">';
+        echo $form;
+        $form = '</form> ';
+        echo $form;
+        $this->html .= '<h1>Upload Form</h1>';
+        $this->html .= $form;
+        
+    }
+    
+    public function post() {
+        echo 'test';
+        print_r($_FILES);
+    }
+}
 ?>
